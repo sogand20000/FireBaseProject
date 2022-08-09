28d09396e3127f30d79da3c520c84ed4b8424dab
somayeh <s.ghanavati94@gmail.com>
2022-08-09 17:34:36 +0430

    Debage Tools

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+console.log('ghgh');
+const App = () => {
+  const x = 5;
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>xcxc</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
